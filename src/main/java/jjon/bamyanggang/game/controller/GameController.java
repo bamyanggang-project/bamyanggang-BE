49dae44df21772ac5ab0aec9fb93feead9bd4d46
oner <950309khs@gmail.com>
2024-05-07 11:43:26 +0900

    게임시작 버튼에 대한 매핑 추가, is_on_game에 대한 값을 대기방에서, 게임시작 버튼을 눌렀을 때 넘겨줌

--- src/main/java/jjon/bamyanggang/game/controller/GameController.java
+++ src/main/java/jjon/bamyanggang/game/controller/GameController.java
@@ -23,12 +23,36 @@ public class GameController {
 
 	@Autowired
 	private GameService gameService;
-
+	
+	// [게임시작 버튼]
+	// int형 room_no 값을 담아 요청 (Front-End)
+	// int형 is_on_game 값을 담아 응답 (Back-End)
+	@GetMapping("getIsOnGame")
+	public ResponseEntity<Map<String, Object>> getIsOnGame(@RequestParam("roomNo") int roomNo) {
+		System.out.println("[게임시작 버튼] Controller 시작!");
+		Map<String, Object> responseBody =new HashMap<String, Object>();
+		try {
+			int getIsOnGame = gameService.getIsOnGame(roomNo);
+			responseBody.put("msg", "[게임시작 버튼] 성공!");
+			responseBody.put("isOnGame", getIsOnGame);
+			
+			// 200
+			return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+		} catch (Exception e) {
+			responseBody.put("msg", "[게임시작 버튼] 실패ㅠ");
+			
+			// 500
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+		}
+	}
+	
+	
 	// [게임시작] 
 	// int형 room_no 값을 담아 요청 (Front-End)
 	// RoomUserInfo 객체에 방에 입장한 사용자들의 정보 값을 담아 응답 (Back-End)
 	@GetMapping("gameStart")
 	public ResponseEntity<Map<String, Object>> gameStart(@RequestParam("roomNo") int roomNo) {
+		System.out.println("[게임시작] Controller 시작!");
 		Map<String, Object> responseBody = new HashMap<String, Object>();
 		try {
 			List<RoomUserInfo> gameStart = gameService.gameStart(roomNo);
@@ -49,6 +73,7 @@ public class GameController {
 	// MafiaRole 객체에 room_no, user_id 값을 담아 요청 (Front-End)
 	@PostMapping("vote")
 	public ResponseEntity<Map<String, Object>> vote(@RequestBody MafiaRole mafiaRole) {
+		System.out.println("[투표] Controller 시작!");
 		Map<String, Object> responseBody =new HashMap<String, Object>();
 		try {
 			gameService.votePlus(mafiaRole);
@@ -69,6 +94,7 @@ public class GameController {
 	// Map객체에 result, user_id, user_nicknm 값을 담아 응답 (Back-End)
 	@GetMapping("resultVote")
 	public ResponseEntity<Map<String, Object>> resultVote (@RequestParam("roomNo") int roomNo) {
+		System.out.println("[인게임] Controller 시작!");
 		Map<String, Object> responseBody = new HashMap<String, Object>();
 		try {
 			Map<String, Object> resultVote = gameService.resultVote(roomNo);
@@ -89,6 +115,7 @@ public class GameController {
 	// MafiaRole 객체에 room_no, user_id 값을 담아 요청 (Front-End)
 	@PostMapping("gameOut")
 	public ResponseEntity<Map<String, Object>> gameOut(@RequestBody MafiaRole mafiaRole) {
+		System.out.println("[게임나가기] Controller 시작!");
 		Map<String, Object> responseBody = new HashMap<String, Object>();
 		try {
 			gameService.gameOut(mafiaRole);
