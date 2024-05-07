84fa3056054f2a04271cd64d282aa4c44caec793
haha0ha <haha0hazzang@gmail.com>
2024-05-07 13:44:57 +0900

    reply sql문 수정

--- src/main/java/jjon/bamyanggang/community/controller/CommunityController.java
+++ src/main/java/jjon/bamyanggang/community/controller/CommunityController.java
@@ -20,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import jjon.bamyanggang.community.service.CommunityService;
 import jjon.bamyanggang.model.CommunityDto;
+import jjon.bamyanggang.reply.service.ReplyService;
 
 
 @RestController
@@ -29,59 +30,62 @@ public class CommunityController {
 	@Autowired
 	private CommunityService communityService;
 	
+	@Autowired
+	private ReplyService replyService;
+	
 	//자게 글쓰기
 	@PostMapping("/communitywrite")					//test시 : ModelAttribute : 동기적
-	public ResponseEntity<Integer> communitywrite(@RequestBody CommunityDto community,
-												  @RequestPart(value = "imgfile", required = false) MultipartFile file
+	public ResponseEntity<Integer> communitywrite(@RequestBody CommunityDto community
+												  //@RequestPart(value = "imgfile", required = false) MultipartFile file
 												  )throws Exception{
 												//		input의 name값이랑 일치시키기
 		
-		String filename = file.getOriginalFilename(); 
-		int size = (int)file.getSize(); //첨부파일 크기 (Byte)
-		
-		//파일 저장 디렉토리 경로 :: 어떻게 잡을지 협의
-	    String path = "C:/upload"; 
-	    
-		//확인
-	    System.out.println("file : "+file);
-	    System.out.println("filename : "+filename);
-	    System.out.println("size :" + size);
-		System.out.println("Path :" + path);
-	    
+//		String filename = file.getOriginalFilename(); 
+//		int size = (int)file.getSize(); //첨부파일 크기 (Byte)
+//		
+//		//파일 저장 디렉토리 경로 :: 어떻게 잡을지 협의
+//	    String path = "C:/upload"; 
+//	    
+//		//확인
+//	    System.out.println("file : "+file);
+//	    System.out.println("filename : "+filename);
+//	    System.out.println("size :" + size);
+//		System.out.println("Path :" + path);
+//	    
 		int result = 0;
-		String newfilename="";
-		
-		if(file != null && !file.isEmpty()) { //첨부파일 전송된 경우
-			
-			//파일중복문제
-			String extension = filename.substring(filename.lastIndexOf("."), filename.length()); //확장자 파싱
-			System.out.println("extension:"+extension);  // extension: 확장자
-			
-			UUID uuid = UUID.randomUUID();
-			
-			newfilename = uuid.toString() + extension;
-			System.out.println("newfilename:"+newfilename);
-			
-			if(size > 100000){ //100KB (큰건가.. 작은것같은디)
-				result = 2;
-				return new ResponseEntity<>(result, HttpStatus.OK);
-				
-				//아래의 확장자명이 아닌 경우(이미지x) 
-			}else if(!extension.equals(".jpg")  &&
-					 !extension.equals(".jpeg") &&
-					 !extension.equals(".gif")  &&
-					 !extension.equals(".png") 
-					){
-				result = 3;
-				return new ResponseEntity<>(result, HttpStatus.OK);
-			}
-			//해당경로에 파일 저장
-			file.transferTo(new File(path + "/" + newfilename));
-		}
-		
-
-		
-			community.setImg(newfilename);
+//		String newfilename="";
+//		
+//		if(file != null && !file.isEmpty()) { //첨부파일 전송된 경우
+//			
+//			//파일중복문제
+//			String extension = filename.substring(filename.lastIndexOf("."), filename.length()); //확장자 파싱
+//			System.out.println("extension:"+extension);  // extension: 확장자
+//			
+//			UUID uuid = UUID.randomUUID();
+//			
+//			newfilename = uuid.toString() + extension;
+//			System.out.println("newfilename:"+newfilename);
+//			
+//			if(size > 100000){ //100KB (큰건가.. 작은것같은디)
+//				result = 2;
+//				return new ResponseEntity<>(result, HttpStatus.OK);
+//				
+//				//아래의 확장자명이 아닌 경우(이미지x) 
+//			}else if(!extension.equals(".jpg")  &&
+//					 !extension.equals(".jpeg") &&
+//					 !extension.equals(".gif")  &&
+//					 !extension.equals(".png") 
+//					){
+//				result = 3;
+//				return new ResponseEntity<>(result, HttpStatus.OK);
+//			}
+//			//해당경로에 파일 저장
+//			file.transferTo(new File(path + "/" + newfilename));
+//		}
+//		
+//
+//		
+//			community.setImg(newfilename);
 			result = communityService.communityInsert(community);//insert되면 result==1 리턴
 			
 			return new ResponseEntity<>(result, HttpStatus.OK);//성공하면 1리턴
@@ -169,7 +173,9 @@ public class CommunityController {
 	//자게 글 삭제(로그인확인)
 	@DeleteMapping("/communitydelete/{postNo}")
 	public ResponseEntity<Integer> communitydelete(@RequestBody CommunityDto community)throws Exception{
+//	public ResponseEntity<Integer> communitydelete(@PathVariable("postNo") int postNo)throws Exception{
 		System.out.println("삭제 들어옴");
+//		int result = communityService.communityDelete(postNo);
 		int result = communityService.communityDelete(community.getPostNo());
 		return new ResponseEntity<>(result,HttpStatus.OK);
 		//1 리턴 성공 , 0은 실패?
