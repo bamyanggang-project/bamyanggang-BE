02f55ccbc204b71995a84a7f1b216d03501f0cc6
haha0ha <haha0hazzang@gmail.com>
2024-05-09 16:21:02 +0900

    컨트롤러 @RequestMapping api 추가 수정

--- src/main/java/jjon/bamyanggang/community/controller/CommunityController.java
+++ src/main/java/jjon/bamyanggang/community/controller/CommunityController.java
@@ -24,7 +24,7 @@ import jjon.bamyanggang.reply.service.ReplyService;
 
 
 @RestController
-@RequestMapping("/community")
+@RequestMapping("api/community")
 public class CommunityController {
 	
 	@Autowired
