319dc751193974e084e6046012d9c792f9b2c412
Kimdongkyu <151708233+owencity@users.noreply.github.com>
2024-05-28 14:33:41 +0900

    Update MemberController.java

--- src/main/java/jjon/bamyanggang/member/MemberController.java
+++ src/main/java/jjon/bamyanggang/member/MemberController.java
@@ -4,9 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
